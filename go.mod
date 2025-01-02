2347b9221b34c285f3207d4c64b7db2f0428f872
Daniel Ojo-Williams <ojowilliamsdaniel@gmail.com>
2025-01-02 13:19:33 +0100

    chore: gordle init setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/daniel-ojo-williams/gordle
+
+go 1.23.3
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/daniel-ojo-williams/gordle/gordle"
+
+func main() {
+	game := gordle.New()
+	game.Play()
+}
