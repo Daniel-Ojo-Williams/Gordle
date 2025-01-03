b1d1b2f7eea19da83fcfe9f54fb0f7c310ff5418
Daniel Ojo-Williams <ojowilliamsdaniel@gmail.com>
2025-01-03 21:32:43 +0100

    feat: add git ignore

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,14 +9,20 @@ import (
 )
 
 func main() {
-	corpus, err := gordle.ReadCorpus("./corpus/english.txt")
+	file := flag.String("file", "", "file to read corpus from")
+	flag.Parse()
+	if *file == "" {
+		fmt.Fprint(os.Stderr, "please provide a valid corpus file\n")
+		return
+	}
+	corpus, err := gordle.ReadCorpus(*file)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "unable to read corpus file: %s", err.Error())
+		fmt.Fprintf(os.Stderr, "unable to read corpus file: %s\n", err.Error())
 		return
 	}
 	game, err := gordle.New(os.Stdin, corpus, 6)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "unable to start game: %s", err.Error())
+		fmt.Fprintf(os.Stderr, "unable to start game: %s\n", err.Error())
 		return
 	}
 	game.Play()
